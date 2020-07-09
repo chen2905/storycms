ba12768e5900ba2242fdc2283b7a80ec66b2b485
chen gao <c.gao@ecj.com.au>
2020-07-09 16:49:15 +1000

    second commit

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
 const express = require('express')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-//const connectDB = require('./config/db')
 
 //Load config
 
 dotenv.config({ path: './config/config.env'})
 
+connectDB()
+
 const app = express()
 
+if (process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
 const PORT = process.env.PORT || 3000
-
-
 
 app.listen(
     PORT, 
