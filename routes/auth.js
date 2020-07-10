80843f23d6fd6892a0420bad306305b3dccdc5a2
Chen Gao <maxlin2905@gmail.com>
2020-07-10 22:34:30 +1000

    200710 1034

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
+  
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-//const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-//const Story = require('../models/Story')
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
-// @desc   Auth with Google
-// @route   GET / auth / google
-router.get('/google',passport.authenticate('google',{scope: ['profile']}))
-
-//@desc google auth callback
-//@route get /auth/google/callback
-router.get('/google/callback',passport.authenticate('google',{failureRedirect: '/'},
-(req,res)=>{
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
     res.redirect('/dashboard')
-}
-))
+  }
+)
+// @desc  Logout user
+//@route /auth/logout
+router.get('/logout',(req,res)=>{
+  req.logout()
+  res.redirect('/')
+})
+
 
 
 module.exports = router
