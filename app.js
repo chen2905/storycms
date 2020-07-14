be489a6b6bc702d161ce70836a2f1852afdfae3b
chen gao <c.gao@ecj.com.au>
2020-07-14 20:58:51 +1000

    200714

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require ('express-handlebars')
+const methodOverride = require('method-override')
 const passport =require('passport')
 const session = require('express-session')
 const MongoStore= require('connect-mongo')(session)
@@ -22,14 +23,22 @@ const app = express()
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-
+//method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
 //logging
 if (process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
 }
 
 //handlebars helper
-const {formatDate} = require('./helpers/hbs.js')
+const {formatDate,stripTags,truncate,editIcon,select} = require('./helpers/hbs.js')
 
 //handlebars
 app.engine(
@@ -37,6 +46,10 @@ app.engine(
     exphbs({
         helpers:{
             formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select,
         },
         defaultLayout:'main',
         extname: '.hbs',
@@ -54,6 +67,13 @@ store: new MongoStore({mongooseConnection:mongoose.connection})
 //passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// set global var
+app.use(function  (req,res,next){
+    res.locals.user =req.user || null
+    next()
+})
+
 // static folder
 app.use(express.static(path.join(__dirname,'public')))
 //Routes
